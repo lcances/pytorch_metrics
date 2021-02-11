50a8669ef638f2d369fc2bceb7cd08c6b7608e48
leocances <leocances@gmail.com>
2021-02-11 16:16:11 +0100

    test change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metric_utils',
-    version='1.0.0',
+    version='2.0.0',
     url='https://github.com/leocances/pytorch_metrics.git',
     author='Leo Cances',
     author_email='leocances@gmail.com',
